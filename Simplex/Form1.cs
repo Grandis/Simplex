24c5f1b642e90cfaf1a05c7db1892ff79246cf98
Grandis <grandis.kepr@gmail.com>
2012-11-21 23:35:31 +0200

    Реализация симплекс метода для заданной матрицы. Без ввода значений в форме.

--- Simplex/Form1.cs
+++ Simplex/Form1.cs
@@ -15,5 +15,101 @@ namespace Simplex
         {
             InitializeComponent();
         }
+
+        double L, x1, x2, x3, x4, x5, x6;
+        int x, y;
+        double[,] mainMatrix = null;
+        double[,] firstMatrix = 
+            {
+                {5, 2, 3, 1, 0, 0,10},
+                {1, 6, 2, 0, 1, 0, 20},
+                {4, 0, 3, 0, 0, 1, 18},
+                {-6, -5, -9, 0, 0, 0, 0}
+            };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            simplexCount();
+        }
+
+        public void simplexCount()
+        {
+            // исходное уравнение
+            L = 6 * x1 + 5 * x2 + 9 * x3;
+
+            x = Convert.ToInt32(boundsNumber.Text) + 1;
+            y = x + Convert.ToInt32(variablesNumber.Text);
+
+            // Значение функции L для начального решения
+            //double[] xBegin = { 0, 0, 0, 25, 20, 18 };
+
+            if (mainMatrix != null) firstMatrix = mainMatrix;
+
+            // наименьший элемент в L строке
+            double minCollumn = firstMatrix[3, 0];
+            int minCollumnIndex = 0;
+            for (int j = 0; j < 7; j++)
+            {
+                if (firstMatrix[3, j] < minCollumn)
+                {
+                    minCollumn = firstMatrix[3, j];
+                    minCollumnIndex = j;
+                }
+            }
+            //***************
+
+            // наименьший эллемент в найденном столбце
+            int minRowIndex = 0;
+            double minRow = firstMatrix[0, 6] / firstMatrix[0, minCollumnIndex];
+            for (int i = 0; i < 3; i++)
+            {
+                if (firstMatrix[i, 6] / firstMatrix[i, minCollumnIndex] < minRow)
+                {
+                    minRow = firstMatrix[i, 6] / firstMatrix[i, minCollumnIndex];
+                    minRowIndex = i;
+                }
+            }
+            //********************
+            double minRowCol = firstMatrix[minRowIndex, minCollumnIndex];
+            //MessageBox.Show(minRowCol.ToString());
+
+            // Делим элементы строки
+            for (int j = 0; j < 7; j++)
+            {
+                firstMatrix[minRowIndex, j] /= minRowCol;
+            }
+            //*******************
+            //MessageBox.Show(minRowIndex.ToString());
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i != minRowIndex)
+                {
+                    double tempMinCol = firstMatrix[i, minCollumnIndex];
+                    for (int j = 0; j < 7; j++)
+                    {
+                        firstMatrix[i, j] -= firstMatrix[minRowIndex, j] * tempMinCol;
+                    }
+                }
+            }
+
+            bool check = true;
+            for (int j = 0; j < y - 1; j++)
+            {
+                if (firstMatrix[x - 1, j] < 0) check = false;
+            }
+
+            if (check)
+            {
+                L = firstMatrix[x - 1, y - 1];
+                MessageBox.Show("Целевая функция равна " + L.ToString());
+            }
+
+            else
+            {
+                mainMatrix = firstMatrix;
+                simplexCount();
+            }
+        }
     }
 }
